e2e3f75c40e62e6c9dec0f8d74475cd01729942d
chenderson17 <60629369+chenderson17@users.noreply.github.com>
2024-01-16 20:38:23 -0800

    Updated footer layout

--- timetrackerwebapp/src/Components/Footer.js
+++ timetrackerwebapp/src/Components/Footer.js
@@ -5,18 +5,29 @@ export default function Footer(){
   return(
  <section className="footer">
   <div class="footer-contact">
-   <h3>Contact</h3>
-    <h3>Adress</h3>
+    <div>
+   <h3>Get In Touch</h3>
+    <p>Address</p>
     <address>5555 NE LN Way, 0000 City,State</address>
-    <h3>Phone</h3>
+    <p>Phone</p>
     <p>(555)-555-5555</p>
+    </div>
   </div>
   <div className="footer-socials">
-   
+    <div>
+    <h3>Follow Us</h3>
+    </div>
+    <div>
+      <a href="/">
+    <box-icon type='logo' name='facebook' color="white" size="md"></box-icon>
+    </a>
+    <box-icon name='instagram' type='logo' color="white" size="md" ></box-icon>
+    </div>
   </div>
 
   <div className="nav-summary">
-    <h3>Navigation</h3>
+    <div>
+    <h3>Links</h3>
      <ul className='footer-navbar'>
          <li>
           <a className="navlink">Home</a>
@@ -31,12 +42,13 @@ export default function Footer(){
           <a className="navlink">Contact Us</a>
          </li>
     </ul>
+    </div>
   </div>
   <div className="privacy-policy">
-    <h3>Privacy</h3>
+    
   </div>
   <div className="copyright">
-   <p>&#169; Copyright TimeTracker 2024</p>
+   <p>&#169; Copyright TimeTracker 2024 | <a href="/">Privacy Policy</a></p>
   </div>
  </section>
   )
