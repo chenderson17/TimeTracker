fc8b7ad89a0de805b3ac140b0d2994dab9ea6e46
c h <carolinehen223@gmail.com>
2024-02-02 15:56:42 -0800

    Added pricing radio to switch between monthly and yearly

--- timetrackerwebapp/src/Components/Pricing.js
+++ timetrackerwebapp/src/Components/Pricing.js
@@ -5,6 +5,19 @@ export default function Pricing(){
   <>
    <Header signUpBtnDisplay={true} signInBtnDisplay={true} />
     <h2> <span className='orange'>Flexible</span> Pricing to Fit Your Business Needs</h2>
+    <form>
+    <fieldset className='pricing-radio'>
+        <div className='pricing-radio-monthly-container'>
+            <input type='radio' name='price-radio' value='monthly-pricing' />
+            <label for='monthly'>Monthly</label>
+        </div>
+
+        <div className='pricing-radio-yearly-container'>
+            <input type='radio' name='price-radio' value='yearly-pricing' />
+            <label for='yearly'>Yearly</label>
+        </div>
+    </fieldset>
+    </form>
   </>
  )
 }
